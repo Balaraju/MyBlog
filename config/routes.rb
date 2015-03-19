65b82bee9820e7c720f88018cc6486ab33a08e1c
balaraju <vankalabalaraju@gmail.com>
2015-03-20 00:45:19 +0530

    Balaraju - Added Comments to Post

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'welcome#index'
    devise_for:users, controllers: {registrations: 'registrations', sessions: 'sessions'}
-
+  resources :posts do 
+    resources :comments
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
